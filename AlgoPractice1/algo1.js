ba60c5634d23b59a25ef7324d6ebb1b55203bf85
DriftingShade <sarus333@yahoo.com>
2024-09-17 09:29:30 -0600

    finished algo1 practice

--- AlgoPractice1/algo1.js
+++ AlgoPractice1/algo1.js
@@ -31,3 +31,91 @@ const firstPlusLength = (arr) => {
 
 console.log(firstPlusLength([1, 2, 3, 4, 5]));
 console.log(firstPlusLength([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+// 4. Values Greater than Second
+// For [1,3,5,7,9,13], print values that are greater than its 2nd value. Return how many values this is.
+let arr = [1, 3, 5, 7, 9, 13];
+const valuesGreaterThanSecond = (arr) => {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[1]) {
+      count++;
+    }
+  }
+  return count;
+};
+console.log(valuesGreaterThanSecond(arr));
+
+// 5. Values Greater than Second, Generalized
+// Write a function that accepts any array, and returns a new array with the array values that are greater than its 2nd value. Print how many values this is.
+
+const valuesGreaterThanSecondGeneralized = (arr) => {
+  let count = 0;
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[1]) {
+      count++;
+      newArr.push(arr[i]);
+    }
+  }
+  console.log(count);
+  return newArr;
+};
+console.log(valuesGreaterThanSecondGeneralized([1, 3, 5, 7, 9, 13]));
+console.log(
+  valuesGreaterThanSecondGeneralized([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+);
+
+// 6. This Length, That Value
+// Given two numbers, return array of length num1 with each value num2. Print "Jinx!" if they are the same.
+
+const thisLengthThatValue = (num1, num2) => {
+  let arr = [];
+  if (num1 === num2) {
+    console.log("Jinx!");
+  }
+  for (let i = 0; i < num1; i++) {
+    arr.push(num2);
+  }
+  return arr;
+};
+
+console.log(thisLengthThatValue(3, 3));
+console.log(thisLengthThatValue(4, 5));
+console.log(thisLengthThatValue(5, 3));
+
+// 7. Fit the First Value
+// Your function should accept an array. If value at [0] is greater than array’s length, print "Too big!"; if value at [0] is less than array’s length,
+// print "Too small!"; otherwise print "Just right!".
+
+const fitFirstValue = (arr) => {
+  if (arr[0] > arr.length) {
+    console.log("Too big!");
+  } else if (arr[0] < arr.length) {
+    console.log("Too small!");
+  } else {
+    console.log("Just right!");
+  }
+};
+
+fitFirstValue([5, 3, 4, 5, 6]);
+fitFirstValue([2, 3, 4, 5, 6]);
+fitFirstValue([50, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+// 8. Fahrenheit to Celsius
+// Kelvin wants to convert between temperature scales. Create fahrenheitToCelsius(fDegrees) that accepts a number of degrees in Fahrenheit,
+// and returns the equivalent temperature as expressed in Celsius degrees.
+
+const fahrenheitToCelsius = (fDegrees) => {
+  return (fDegrees - 32) * (5 / 9);
+};
+console.log(fahrenheitToCelsius(72));
+
+// 9. Celsius to Fahrenheit
+// Create celsiusToFahrenheit(cDegrees) that accepts number of degrees Celsius, and returns the equivalent temperature expressed in Fahrenheit degrees.
+
+const celsiusToFahrenheit = (cDegrees) => {
+  return cDegrees * (9 / 5) + 32;
+};
+console.log(celsiusToFahrenheit(22));
+console.log(celsiusToFahrenheit(0));
