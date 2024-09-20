23fb088a1d4380044c82ca0e29e0a0c0e911ee8b
DriftingShade <sarus333@yahoo.com>
2024-09-19 20:31:28 -0600

    Refactoring

---
+++ AlgoNotes/ObjectsAndArrays.js
@@ -0,0 +1,24 @@
+// Objects and Arrays Complexity
+const person = {
+  firstName: "Jacob",
+  lastName: "Hill",
+};
+
+// Insertion - O(1)
+// Removal - O(1)
+// Access - O(1)
+// Searching - O(n)
+// Object.keys - O(n)
+// Object.values - O(n)
+// Object.entries - O(n)
+
+const odd = [1, 3, 5, 7, 9];
+
+// Insertion/remove at the end of the array - O(1)
+// Insertion/remove at the beginning of the array - O(n)
+// Insertion/remove at the middle of the array - O(n)
+// Access - O(1)
+// Searching - O(n)
+// Push/Pop - O(1)
+// Shift/Unshift/Concat/Slice/Splice - O(n)
+// forEach/map/filter/reduce/etc. - O(n)
